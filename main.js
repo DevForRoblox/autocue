3c23644a88b8b31ec396847f9ffd8bb61b131f9b
DevForRoblox <66915709+DevForRoblox@users.noreply.github.com>
2021-01-28 14:58:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -18,15 +18,38 @@ function smoothScroll(target, duration) {
     requestAnimationFrame(loop);
 }
 
-  let start = document.getElementById("start")
-  let input = document.getElementById("input")
-  let text = document.getElementById("text")
-  let title = document.getElementById("title")
-  let scrollDuration = document.getElementById("scrollDuration")
+let start = document.getElementById("start")
+let input = document.getElementById("input")
+let text = document.getElementById("text")
+let title = document.getElementById("title")
+let scrollDuration = document.getElementById("scrollDuration")
 
-  input.value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu pretium quam, nec faucibus enim. Maecenas tincidunt porttitor mi a placerat. Quisque eu feugiat diam, non consectetur felis. Nam a justo purus. Integer augue mauris, lacinia id ipsum in, convallis iaculis arcu. In sed nunc ac urna malesuada mollis nec vitae turpis. Aliquam erat volutpat. Nam quis ligula nisl. Fusce elementum, nulla dapibus faucibus pulvinar, orci purus commodo lectus, mattis maximus neque dui sit amet lectus. In faucibus faucibus ligula, sed consectetur magna aliquam eu. Nulla facilisi. Nam et dui lobortis, faucibus felis et, luctus tortor. Phasellus justo odio, interdum."
-  
-  scrollDuration.value = 10
+input.value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu pretium quam, nec faucibus enim. Maecenas tincidunt porttitor mi a placerat. Quisque eu feugiat diam, non consectetur felis. Nam a justo purus. Integer augue mauris, lacinia id ipsum in, convallis iaculis arcu. In sed nunc ac urna malesuada mollis nec vitae turpis. Aliquam erat volutpat. Nam quis ligula nisl. Fusce elementum, nulla dapibus faucibus pulvinar, orci purus commodo lectus, mattis maximus neque dui sit amet lectus. In faucibus faucibus ligula, sed consectetur magna aliquam eu. Nulla facilisi. Nam et dui lobortis, faucibus felis et, luctus tortor. Phasellus justo odio, interdum."
+
+scrollDuration.value = 10
+
+let filesInput = document.getElementById("fileInput");
+let textArea = document.getElementById("input");
+
+filesInput.onchange = function() {
+  let files = filesInput.files;
+ 
+  if(files.length == 0) return;
+
+  const file = files[0];
+
+  let reader = new FileReader();
+
+  reader.onload = (e) => {
+      const file = e.target.result;
+      const lines = file.split(/\r\n|\n/);
+      textArea.value = lines.join('\n');
+  };
+
+  reader.onerror = (e) => alert(e.target.error.name);
+
+  reader.readAsText(file); 
+}
 
 start.onclick = function() {
 
@@ -62,6 +85,7 @@ start.onclick = function() {
   text.innerHTML = input.value
   scrollDuration.style.display = "none"
   title.remove()
+  filesInput.remove()
   input.remove()
   start.remove()
 
